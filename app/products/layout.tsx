857b6424b40e8059661340aa6e6f8c494787326c
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-22 19:07:47 +0700

    first commit

---
+++ app/products/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Products",
+};
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="p-10">{children}</div>;
+};
+
+export default layout;
